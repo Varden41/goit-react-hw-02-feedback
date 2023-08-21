eddbc102cdb2788c9ed7561cebe26df269ec52f4
Varden41 <arturpavlenko14@gmail.com>
2023-08-21 16:06:01 +0300

    штшешфд сщььше

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,36 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import Section from './Section/Section';
+import FeedbackOption from './FeedbackOption/FeedbackOption';
+import Statistics from './Statistics/Statistics';
+import Notification from './Notification/Notification';
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  // handleIncrement = () => {this.setState(prevState => {
+  //   value: prevState.value + 1;
+  // });}
+  // handleDecrement = () => {this.setState(prevState => {
+  //   value: prevState.value - 1;
+  // });}
+
+  render() {
+    return (
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        <Section title="Please leave feedback"></Section>
+      </div>
+    );
+  }
+}
+export default App;
